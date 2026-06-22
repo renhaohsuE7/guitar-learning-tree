cb5dadaead36526e1615b46366e25036556439dd
renhaohsu <renhaohsu@thinktronltd.com>
2026-06-22 20:47:59 +0800

    feat: add instruments registry

---
+++ instruments.js
@@ -0,0 +1,8 @@
+// Single source of truth for the instruments shown on the landing page
+// and in each instrument page's nav bar. Add a new instrument here +
+// one data-<id>.js + one <id>.html to extend the site.
+const INSTRUMENTS = [
+  { id: 'guitar', label: '電吉他', emoji: '🎸', page: 'guitar.html' },
+  { id: 'drum',   label: '鼓',     emoji: '🥁', page: 'drum.html'   },
+  // 日後擴充:{ id: 'bass', label: '貝斯', emoji: '🎵', page: 'bass.html' },
+];
